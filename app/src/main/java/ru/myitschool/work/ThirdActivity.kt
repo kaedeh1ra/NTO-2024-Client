cc85341c19fae7881e802f2d39db6addfcdd81ce
sashaviee <162354543+sashaviee@users.noreply.github.com>
2024-12-07 20:15:07 +0400

    my favorite sing is "drip"

--- app/src/main/java/ru/myitschool/work/ThirdActivity.kt
+++ app/src/main/java/ru/myitschool/work/ThirdActivity.kt
@@ -15,6 +15,10 @@ class ThirdActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
+
         }
     }
 }
